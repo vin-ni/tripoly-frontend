9e486f22a67f246b3ec9039dd5a4dfc4df2e462d
Vinzenz Aubry <vinzenzaubry@gmail.com>
2021-12-16 20:08:59 +0200

    added css base

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   css: [],
 
   styleResources: {
-    scss: ['~assets/scss/mixins.scss', '~assets/scss/variables.scss'],
+    scss: ['~assets/scss/main.scss'],
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
