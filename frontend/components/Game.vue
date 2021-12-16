eb997dfb7186074646d422b27ae771b75677ff17
Vinzenz Aubry <vinzenzaubry@gmail.com>
2021-12-17 01:04:35 +0200

    added a lot for the game

---
+++ frontend/components/Game.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="game">
+    <div class="game-wrapper">
+      <div class="title">
+        <h1>Game</h1>
+        <div class="gamge-triggers">
+          <button>Sync</button>
+          <button>Join</button>
+          <button>Leave</button>
+        </div>
+      </div>
+    </div>
+    <div class="field-info">
+      <p>Field 17</p>
+    </div>
+    <div class="company-info">
+      <div class="project content five-row">
+        <div class="company span-two-first">
+          <h3>Title Company</h3>
+          <p>
+            For this game, named Tripoly, we forget the bank, the loans,and the
+            race to become rich #capitalism. We revisit theMonopoly's mechanism,
+            we take inspiration from it to makethe project funny and
+            interactive. Some of you may think about this other game :
+            Anti-Monopoly. Let's say that Tripoly is the NFT's sibling of
+            theAnti.
+          </p>
+        </div>
+        <div class="ar span-two-last">
+          <h3>View object in AR</h3>
+          <div class="qr-code">
+            <img src="@/assets/img/sampleqr.png" alt="" />
+            <p>iPhone</p>
+          </div>
+        </div>
+      </div>
+      <div class="content cta-text five-row">
+        <h3 class="span-four">
+          If you like the project and wish to contribute, please support it by
+          buying the token.
+        </h3>
+      </div>
+      <div class="content">
+        <button>Support</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'GameComp',
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  mounted() {},
+  created() {},
+  methods: {},
+}
+</script>
--- frontend/components/Menu.vue
+++ frontend/components/Menu.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="menu">
-    <a href="https://github.com/schwittlick/tzconnect-hackathon">Github</a>
     <a href="mailto:info@schwittlick.net?subject=Tripoly">Contact</a>
+    <a href="https://github.com/schwittlick/tzconnect-hackathon">Github</a>
     <p>Project for the TzConnect Hackathon 2021</p>
   </nav>
 </template>
