3a54201c141a4de40ca5225ae9d2ee94b002e4c6
Vinzenz Aubry <vinzenzaubry@gmail.com>
2021-12-16 20:57:33 +0200

    right font

--- frontend/components/Menu.vue
+++ frontend/components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="menu">
-    <a href="https://github.com/schwittlick/tzconnect-hackathon">About</a>
+    <a href="https://github.com/schwittlick/tzconnect-hackathon">Github</a>
     <a href="https://github.com/schwittlick/tzconnect-hackathon">Github</a>
     <a href="mailto:info@schwittlick.net?subject=Tripoly">Contact</a>
     <p>Project for the TzConnect Hackathon 2021</p>
@@ -9,6 +9,7 @@
 
 <script>
 export default {
+  name: 'MenuComp',
   props: {},
   data() {
     return {}
