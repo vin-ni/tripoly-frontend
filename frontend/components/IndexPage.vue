a7e35fbcf0e6ee2f78e0ad4b14cd5bf5265a3034
Marcel Schwittlick <info@schwittlick.net>
2021-12-18 20:39:50 +0100

    update text

--- frontend/components/IndexPage.vue
+++ frontend/components/IndexPage.vue
@@ -5,19 +5,14 @@
       <section class="five-row">
         <div class="text columns-two span-four">
           <p>
-            HALLO We want to create a decentralized board-game on Tezos, toeducate
-            people about renewable and sustainable projects ina playful and
-            interactive way. We want to disrupt the game idea of Monopoly and
-            turn itsvalues around, from capitalism and re-shape it for the
-            futureof web3 to an open source knowledge game using theconventions
-            and the features of the metaverse.
+            Tripoly is a round based, decentralized and multiplayer board-game on the Tezos Blockchain. The aim of 
+            this game is to educate about renewable and sustainable projects in a simple, playful and interactive way. 
             <br />
-            For this game, named Tripoly , we forget the bank, the loans,and the
-            race to become rich #capitalism. We revisit theMonopoly's mechanism,
-            we take inspiration from it to makethe project funny and
-            interactive. Some of you may think about this other game :
-            Anti-Monopoly. Let's say that Tripoly is the NFT's sibling of the
-            Anti.
+            Some of you may know the Anti-Monopoly games. We love the idea of being Tripoly is another disruptive form 
+            of Anti-monopoly. We turn the values game of Monopoly around and re-shape it for the future of web3 : from a 
+            capitalistic dynamic to an open source knowledge game using the conventions and the features of the metaverse. 
+            <br />
+            Are you ready? 
           </p>
         </div>
       </section>
@@ -45,55 +40,6 @@
               The game interface is an equilateral triangle, Composed of 18
               juxtaposed triangular fields. The game is organized as follows:
             </p>
-            <ul>
-              <li>People can join the game</li>
-              <li>Sync their profile to the game</li>
-              <li>
-                Roll the dice Step forward on the board game, clockwise, only on
-                the edge of the board
-              </li>
-              <li>
-                Once the player steps on a field, they have access to the
-                content of thisfield.
-              </li>
-              <li>In Tripoly, each field is a sustainable project</li>
-              <li>
-                At this point, people discover the project and have different
-                options ofsupporting it
-              </li>
-              <li>
-                Each project has 3 levels of support: Research, Prototype,
-                Start-up
-              </li>
-              <li>Each level of support has a different price</li>
-              <li>
-                Each project has its specific value based on how much the
-                project needs tobe supported
-              </li>
-              <li>
-                Player is shown the project which is saved at each position of
-                the boardgame when rolling the dice and steping on each field
-              </li>
-              <li>
-                Player supports the project by buying the NFT related to its
-                position
-              </li>
-              <li>Player also receives Co2 balance certificates</li>
-              <li>
-                When the player keeps playing and goes over the start, they are
-                rewarded by two bounties:
-                <ul>
-                  <li>Tezos liquidity</li>
-                  <li>
-                    Co2 saved balance: depending of how many projects player
-                    supported, how many NFT bought
-                  </li>
-                  <li>
-                    Player can leave and join the game as much as they want.
-                  </li>
-                </ul>
-              </li>
-            </ul>
           </div>
         </section>
       </VueSlideToggle>
