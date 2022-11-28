965a651d5ea1108f3754b27432d7da2ece428707
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-11-28 09:41:16 +0100

    Update ecosystem.config.js

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,13 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'tripoly',
-      exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
+      exec_mode: 'fork', // needs to be fork for interpreter
+      instances: '1', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
+      interpreter: '/home/forge/.nvm/versions/node/v16.18.0/bin/node', // 'v18.11.0'
       env: {
         HOST: 'localhost',
-        PORT: 3009,
+        PORT: 3002,
       },
     },
   ],
